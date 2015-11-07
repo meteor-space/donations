1885cc5cd2b76cce9716a70a8971d3c869f399bd
Dominik Guzei <dominik.guzei@gmail.com>
2015-11-07 12:22:21 +0100

    updates to improved struct fields definition api

--- packages/base/source/server/value-objects/address.js
+++ packages/base/source/server/value-objects/address.js
@@ -1,12 +1,14 @@
-Space.messaging.Serializable.extend(Donations, 'Address', {});
-
-// EJSON serializable fields
-Donations.Address.fields = {
-  country: Country,
-  zip: String,
-  city: String,
-  street: String
-};
+Space.messaging.Serializable.extend(Donations, 'Address', {
+  // EJSON serializable fields
+  fields: function() {
+    return {
+      country: Country,
+      zip: String,
+      city: String,
+      street: String
+    };
+  }
+});
 
 // Register as EJSON type
 Donations.Address.type('Donations.Address');
--- packages/base/source/server/value-objects/contact.js
+++ packages/base/source/server/value-objects/contact.js
@@ -1,11 +1,13 @@
-Space.messaging.Serializable.extend(Donations, 'Contact', {});
-
-// EJSON serializable fields
-Donations.Contact.fields = {
-  name: String,
-  email: EmailAddress,
-  phone: String
-};
+Space.messaging.Serializable.extend(Donations, 'Contact', {
+  // EJSON serializable fields
+  fields: function() {
+    return {
+      name: String,
+      email: EmailAddress,
+      phone: String
+    };
+  }
+});
 
 // Register as EJSON type
 Donations.Contact.type('Donations.Contact');

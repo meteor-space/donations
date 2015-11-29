4752fa05cfd155c80ebc75d960763856194c35aa
Rhys Bartels-Waller <rhyslbw@gmail.com>
2015-11-29 21:41:05 +1100

    Switch to Space.getenv for default envs

--- packages/app/source/server/dev-data-setup.js
+++ packages/app/source/server/dev-data-setup.js
@@ -1,15 +1,19 @@
+let env = Space.getenv.multi({
+  password: ['EXAMPLE_ORG_PASSWORD', '1234', 'string'],
+  email: ['EXAMPLE_ORG_EMAIL', 'example@email.com', 'string']
+});
 
 let createExampleOrg = function() {
-  let byEmail = { 'emails.address': process.env.EXAMPLE_ORG_EMAIL };
+  let byEmail = { 'emails.address': env.email };
   if (Meteor.users.find(byEmail).count() === 0) {
 
     Donations.app.send(new Donations.RegisterOrganization({
       targetId: new Guid(),
       name: 'My Example Organization',
-      password: new Password(SHA256(process.env.EXAMPLE_ORG_PASSWORD)),
+      password: new Password(SHA256(env.password)),
       country: new Country('AT'),
       contact: new Donations.Contact({
-        email: new EmailAddress(process.env.EXAMPLE_ORG_EMAIL),
+        email: new EmailAddress(env.email),
         name: 'Dominik Guzei',
         phone: '+43 676 9222862'
       })

4b29c8e26ae77f5fb6f4a915c52924e8630058e9
Rhys Bartels-Waller <rhyslbw@gmail.com>
2015-12-09 21:52:47 +1100

    Properly call Space.Error when extending

--- packages/base/source/server/appeals/errors.js
+++ packages/base/source/server/appeals/errors.js
@@ -1,20 +1,17 @@
 Space.Error.extend(Donations, `InvalidAppealState`, {
   Constructor(commandName, currentState) {
-    Space.Error.constructor.call(this);
-    this.message = `Cannot ${commandName} when in ${currentState} state`;
+    Space.Error.call(this, `Cannot ${commandName} when in ${currentState} state`);
   }
 });
 
 Space.Error.extend(Donations, `InvalidPledgeState`, {
   Constructor(commandName, currentState) {
-    Space.Error.constructor.call(this);
-    this.message = `Cannot ${commandName} when in ${currentState} state`;
+    Space.Error.call(this, `Cannot ${commandName} when in ${currentState} state`);
   }
 });
 
 Space.Error.extend(Donations, `PledgeNotFoundError`, {
   Constructor(pledgeId) {
-    Space.Error.constructor.call(this);
-    this.message = `No pledge with id ${pledgeId} found.`;
+    Space.Error.call(this, `No pledge with id ${pledgeId} found.`);
   }
 });

89ceebb5a2b99b5186676bf85530ebb1ac3a9a2f
Darko Mijić <darko.mijic@ebiz.hr>
2016-01-30 14:25:13 +0100

    Update application package configuration

--- packages/domain/package.js
+++ packages/domain/package.js
@@ -15,12 +15,13 @@ Package.onUse(function(api) {
     'check',
     'ecmascript',
     'underscore',
-    'space:vo-user@0.2.1',
-    'space:vo-i18n@0.1.0',
-    'space:vo-numeral@0.1.0',
+    'space:vo-user@0.3.0',
+    'space:vo-i18n@0.2.0',
+    'space:vo-numeral@0.2.0',
     'space:domain@0.2.1',
-    'space:event-sourcing@2.1.0',
-    'space:accounts@0.1.3',
+    'space:event-sourcing@3.0.0',
+    'space:accounts@0.2.0',
+    'space:accounts-ui@0.3.0',
     'donations:base'
   ]);
 

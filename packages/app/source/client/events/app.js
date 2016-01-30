012d5e03cc710ff5a9755bbc8da05bcadc352c3f
Darko Mijić <darko.mijic@ebiz.hr>
2016-01-30 14:24:46 +0100

    Refactor ui events to new Space.ui.Event

--- packages/app/source/client/events/app.js
+++ packages/app/source/client/events/app.js
@@ -1,4 +1,4 @@
-Space.messaging.define(Space.messaging.Event, 'Donations', {
+Space.ui.defineEvents('Donations', {
 
   RouteRequested: { routeName: String, params: Match.Optional(Object) },
   RouteTriggered: { routeName: String, params: Match.Optional(Object) },

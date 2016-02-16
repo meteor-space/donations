93e19b126d421151365524c28599096abf61802e
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-02-17 09:01:45 +1100

    Adds basic security to the restricted Api endpoints
    We can replace this with a more sophisticated approach later

--- packages/app/source/server/apis/appeals-api.js
+++ packages/app/source/server/apis/appeals-api.js
@@ -11,18 +11,30 @@ Space.messaging.Api.extend(Donations, 'AppealsApi', {
   },
 
   _draftAppeal(context, command) {
+    if (context.userId == null) {
+      throw new Meteor.Error('You are not allowed to draft appeals');
+    }
     this.commandBus.send(command);
   },
 
   _updateAppealDraft(context, command) {
+    if (context.userId == null) {
+      throw new Meteor.Error('You are not allowed to update appeal drafts');
+    }
     this.commandBus.send(command);
   },
 
   _makeAppeal(context, command) {
+    if (context.userId == null) {
+      throw new Meteor.Error('You are not allowed to make appeals');
+    }
     this.commandBus.send(command);
   },
 
   _updateAppeal(context, command) {
+    if (context.userId == null) {
+      throw new Meteor.Error('You are not allowed to draft appeals');
+    }
     this.commandBus.send(command);
   },
 
--- packages/app/source/server/apis/org-api.js
+++ packages/app/source/server/apis/org-api.js
@@ -8,10 +8,16 @@ Space.messaging.Api.extend(Donations, 'OrgApi', {
   },
 
   _addLocation(context, command) {
+    if (context.userId == null) {
+      throw new Meteor.Error('You are not allowed to add organisation locations');
+    }
     this.commandBus.send(command);
   },
 
   _updateLocationDetails(context, command) {
+    if (context.userId == null) {
+      throw new Meteor.Error('You are not allowed to update location details');
+    }
     this.commandBus.send(command);
   }
 
